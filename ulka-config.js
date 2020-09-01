b52855d54a98b4d311baccf3a165c3d9beefbc0b
Saurav Gautam <55937631+sauravgautam320@users.noreply.github.com>
2020-09-01 09:46:33 +0545

    Initial commit

---
+++ ulka-config.js
@@ -0,0 +1,37 @@
+// All the configurations are changable
+
+module.exports = {
+  siteMetaData: {
+    title: "UlkaJs",
+    description: "This is a default starter for ulka js",
+    domain: "https://ulka-starter-default.netlify.app/",
+  },
+  buildPath: "build",
+  pagesPath: "pages",
+  templatesPath: "templates",
+  plugins: [
+    "ulka-plugin-reading-time",
+    {
+      resolve: "ulka-plugin-autolink-headers",
+      options: {
+        elements: ["h1", "h2", "h3", "h4"],
+      },
+    },
+    "ulka-plugin-sitemap",
+    "ulka-plugin-retext-smartypants",
+    {
+      resolve: "ulka-plugin-remark-emoji",
+      options: {
+        emoticon: true,
+        padSpaceAfter: true,
+      },
+    },
+    "ulka-plugin-accessible-emoji",
+    "ulka-plugin-rehype-highlight",
+  ],
+  contents: {
+    path: "contents",
+    generatePath: "blog",
+    template: "blog.ulka",
+  },
+};
